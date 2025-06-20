c87c7fb899fbcecf28e5f1961200077e1a927df3
alicortez855 <alicortez6@gmail.com>
2025-06-20 11:45:44 -0700

    oh

--- script.js
+++ script.js
@@ -7,5 +7,7 @@ function setup() //called once when programs starts
 
 function draw() //this runs in a loop to update the displayphp 
 {
-
+    point (150,250);
+    line (0,0,400,400); /* x1 and y1 is the frst piont of line, x2 and ya is the end of piont */
+    rect (100,100,100,100);
 }

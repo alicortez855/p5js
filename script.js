3a6263a188d37aeb0d2fcfa21de5be67dbfe4db2
alicortez855 <alicortez6@gmail.com>
2025-06-20 17:37:03 +0000

    ;lol

---
+++ script.js
@@ -0,0 +1,11 @@
+function setup() //called once when programs starts
+{
+    createCanvas(400,400);
+    background(220);
+}
+
+
+function draw() //this runs in a loop to update the displayphp 
+{
+
+}
